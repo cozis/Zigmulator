a3dbb48718adbad167e42819c7ed0c82f8b5d0ff
cozis <francesco.cozzuto@gmail.com>
2026-06-08 10:39:34 +0200

    trace: add taskSpawned and taskRemoved

--- src/trace.zig
+++ src/trace.zig
@@ -23,6 +23,19 @@ pub const Trace = struct {
         _ = self;
     }
 
+    pub fn taskSpawned(self: *Trace, task_id: TaskID, node: *Node, parent_id: ?TaskID) void {
+        _ = self;
+        _ = task_id;
+        _ = node;
+        _ = parent_id;
+    }
+
+    pub fn taskRemoved(self: *Trace, task_id: TaskID, node: *Node) void {
+        _ = self;
+        _ = task_id;
+        _ = node;
+    }
+
     pub fn beginIO(self: *Trace, disk: bool, source: std.builtin.SourceLocation) PendingTrace {
         _ = self;
         _ = disk;
